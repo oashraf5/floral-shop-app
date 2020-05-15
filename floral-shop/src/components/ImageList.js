61cc917a82dfdb605e72d8e6364e7812c53a399f
devMorgan799 <61382492+devMorgan799@users.noreply.github.com>
2020-05-15 17:20:07 -0400

    imglist updated with pictures

--- floral-shop/src/components/ImageList.js
+++ floral-shop/src/components/ImageList.js
@@ -1,7 +1,41 @@
 
 import img1 from '../assets/easter-eggs-1.jpg';
 
+import img2 from '../assets/appointment2_page.jpg';
+import img3 from '../assets/appointment_page.jpg';
+import img4 from '../assets/bday_event.jpg';
+import img5 from '../assets/dinner2_event.jpg';
+import img6 from '../assets/dinner_event.jpg';
+import img7 from '../assets/funeral2_event.jpg';
+import img8 from '../assets/funeral_event.jpg';
+import img9 from '../assets/home copy.jpg';
+import img10 from '../assets/home_page.png'; v
+import img11 from '../assets/mom_event.jpg';
+import img12 from '../assets/shower_event.jpg';
+import img13 from '../assets/showers.jpg';
+import img14 from '../assets/wed_momday pic.jpg';
+import img15 from '../assets/wedding2_event.jpg';
+import img16 from '../assets/wedding3_event.jpg';
+
+
+
+
+
 let ImageList = [];
 ImageList.push(img1);
-
+ImageList.push(img2);
+ImageList.push(img3);
+ImageList.push(img4);
+ImageList.push(img5);
+ImageList.push(img6);
+ImageList.push(img7);
+ImageList.push(img8);
+ImageList.push(img9);
+ImageList.push(img10);
+ImageList.push(img11);
+ImageList.push(img12);
+ImageList.push(img13);
+ImageList.push(img14);
+ImageList.push(img15);
+ImageList.push(img16);
 export default ImageList;
