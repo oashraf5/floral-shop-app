b11ba9bece3af379958a26c16b0022ff6a1acc2f
KL <dev.lau3@gmail.com>
2020-05-19 07:38:21 -0500

    event component. about us component

--- floral-shop/src/components/Event.js
+++ floral-shop/src/components/Event.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '../App.css';
 
+import wedImg from '../assets/wed_momday pic.jpg';
+import showerImg from '../assets/shower_event.jpg';
+import annImg from '../assets/home copy.jpg';
+import bdayImg from '../assets/bday_event.jpg';
+import funeralImg from '../assets/funeral2_event.jpg';
+import dinnerImg from '../assets/dinner_event.jpg';
+
 export default function Event(props) {
 
     
@@ -12,7 +19,64 @@ export default function Event(props) {
 
     return (  
         <div id={toContainerId}>
-            <p className="designer-desc">Abbey, florist and chief bargain hunter behind the boutique, has made it a real curiosity shop known for its character-filled bouquets, which often incorporate unusual plants and colors. “Our clients are often looking for the style typified by our shop– a little odd but genuine, bouquets that are the stuff of dreams,” They tell us, having decorated catwalks and created sumptuous decor for famous names like the Opéra Garnier, Dior, and Cartier. “We have an overall vision of flowers which we perceive as being a part of the whole, like a plant in an English-style garden”.</p>
+            
+            <div className="event-box">
+                <img className="event-img" src={wedImg} />
+                <div className="event-desc-box">
+                <br /><br /> <br /><br />
+                    <h3>WEDDINGS</h3>
+                    <p className="event-desc">No Matter the type of wedding you have in mind, we can assist in making your dream wedding come true</p>
+                </div>
+            </div>
+            <p className="quote-text1">"Happiness Blossoms From Within"</p>
+            <div className="event-box">
+                <div className="event-desc-box">
+                    <br /><br /><br /><br /><br />
+                    <h3>SHOWERS</h3>
+                    <p className="event-desc">We can assist in the floral designs for your bridal shower, bro-dal shower and baby shower</p>
+                </div>
+                <img className="event-img" src={showerImg} />
+            </div>
+
+            <br /><br /><br />
+
+            <div className="event-box">
+                <img className="event-img" src={annImg} />
+                <div className="event-desc-box">
+                <br /><br /> <br /><br />
+                    <h3>ANNIVERSARY</h3>
+                    <p className="event-desc">Romantic floral arrangements to celebrate your love</p>
+                </div>
+            </div>
+            <p className="quote-text1">"Earth laughs in flowers"</p>
+            <div className="event-box">
+                <div className="event-desc-box">
+                    <br /><br /><br /><br /><br />
+                    <h3>EVENTS</h3>
+                    <p className="event-desc">fresh floral arrangements that will complement any event</p>
+                </div>
+                <img className="event-img" src={bdayImg} />
+            </div>
+
+            <br /><br /><br />
+
+            <div className="event-box">
+                <img className="event-img" src={funeralImg} />
+                <div className="event-desc-box">
+                <br /><br /> <br /><br />
+                    <h3>FUNERALS</h3>
+                    <p className="event-desc">Honor the life of a loved one. We will ensure your flowers are hand-picked with care and respect.</p>
+                </div>
+            </div>
+            <p className="quote-text1">"Where flowers bloom so does hope"</p>
+            <div className="event-box">
+                <div className="event-desc-box">
+                    <br /><br /><br /><br /><br />
+                    <h3>SPECIAL DINNERS</h3>
+                    <p className="event-desc">We will design intimate floral dinner combinations for your personal space</p>
+                </div>
+                <img className="event-img" src={dinnerImg} />
+            </div>
         </div>
     )
 }
